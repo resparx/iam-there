06eded78e933083a8bd19f061791274d811d4e4f
resparx <ramprash.rspx@gmail.com>
2020-08-04 15:51:17 +0530

    add helmet for cdn script

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,9 +36,13 @@ const Layout = ({ children }) => {
 
   return (
     <>
-        <main>{children}</main>
-        <Footer>
-          made with <HeartTwoTone twoToneColor="#eb2f96" /> by Rammmmmm....!
+      <Helmet>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" integrity="sha256-qM7QTJSlvtPSxVRjVWNM2OfTAz/3k5ovHOKmKXuYMO4=" crossorigin="anonymous">
+        </script>
+      </Helmet>
+      <main>{children}</main>
+      <Footer>
+        made with <HeartTwoTone twoToneColor="#eb2f96" /> by Rammmmmm....!
         </Footer>
     </>
   )
