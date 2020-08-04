2f95b48a35ead046274d484680d3befe872efe84
resparx <ramprash.rspx@gmail.com>
2020-08-04 13:02:45 +0530

    normalize

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,43 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+
+const Background = styled.div`
+  height: 100vh;
+  /* width: 100vw; */
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+
+  @keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
+`
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <Background>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      {/* <Image /> */}
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Background>
   </Layout>
 )
 
