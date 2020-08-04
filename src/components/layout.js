2fbc383f1b060bdb974b882c28b49d653f5980a3
resparx <ramprash.rspx@gmail.com>
2020-08-04 15:53:41 +0530

    add react helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styled from 'styled-components';
+import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby"
 import { HeartTwoTone } from '@ant-design/icons';
 
